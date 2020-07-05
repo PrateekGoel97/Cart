df9bedaf595837ff6ad9d7720075a1f9adfad0ee
Prateek Goel <goelprateek97@gmail.com>
2020-07-05 15:45:09 +0530

    add Cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,14 +10,34 @@ class CartItem extends React.Component {
       img: ''
     }
     // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
   }
+
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
+
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
   increaseQuantity = () => {
     // this.state.qty += 1;
     // console.log('this', this.state);
     // setState form 1
     // this.setState({
     //   qty: this.state.qty + 1
-    // });
+    // }, () => {});
 
     // setState form 2 - if prevState required use this
     this.setState((prevState) => {
@@ -27,15 +47,21 @@ class CartItem extends React.Component {
     });
   }
 
-  decreaseQuantity = () =>{
-    this.setState( (prevState) =>{
-        return {
-          qty: prevState.qty-1
-        }
-    })
-  }
+  decreaseQuantity = () => {
+    const { qty } = this.state;
 
+    if (qty === 0) {
+      return;
+    }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
   render () {
+    console.log('render');
     const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
