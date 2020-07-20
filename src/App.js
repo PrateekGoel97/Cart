aefdfd302a903048a492e94171d5f26a55083bc8
Prateek Goel <goelprateek97@gmail.com>
2020-07-20 15:50:22 +0530

    reading data from firebase

--- src/App.js
+++ src/App.js
@@ -1,36 +1,72 @@
 import React from 'react';
 import Cart from './Cart';
 import NavBar from './NavBar';
+import * as firebase from'firebase';
 
 class App extends React.Component {
 
   constructor () {
     super();
     this.state={
-        products:[
-        {
-            price: 25000,
-            title: 'Mobile Phone',
-            qty: 100,
-            img: 'https://images.unsplash.com/photo-1455762279210-ae6b56c7ad7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-            id:1
-        },
-        {
-            price: 56000,
-            title: 'Laptop',
-            qty: 10,
-            img: 'https://images.unsplash.com/photo-1455894127589-22f75500213a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-            id:2
-        },
-        {
-            price: 2000,
-            title: 'Watch',
-            qty: 20,
-            img: 'https://images.unsplash.com/photo-1517463700628-5103184eac47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-            id:3
-        }
-    ]
+        products:[],
+        loading:true
   } 
+}
+
+componentDidMount(){
+  //  firebase
+  //  .firestore()
+  //  .collection('products')
+  //  .get()
+  //  .then((snapshot) =>{
+  //    console.log(snapshot);
+
+  //    snapshot.docs.map((doc)=>{
+  //      console.log(doc.data());
+  //    })
+
+  //    const products = snapshot.docs.map((doc)=>{
+      
+  //     const data=doc.data();
+
+  //     data['id']= doc.id;
+
+  //     return data;
+  //    })
+
+  //    this.setState({
+  //      products:products,
+  //      loading:false
+  //    })
+
+  //  })
+
+  firebase
+   .firestore()
+   .collection('products')
+   .onSnapshot((snapshot) =>{
+    console.log(snapshot);
+
+    snapshot.docs.map((doc)=>{
+      console.log(doc.data());
+    })
+
+    const products = snapshot.docs.map((doc)=>{
+     
+     const data=doc.data();
+
+     data['id']= doc.id;
+
+     return data;
+    })
+
+    this.setState({
+      products:products,
+      loading:false
+    })
+
+  })
+
 }
 
 handleIncrease = (product) =>{                           // getting product as a parameter
@@ -56,7 +92,8 @@ handleDecrease =( product) =>{
     products[index].qty -= 1;
 
     this.setState({
-        products:products
+        products:products,
+        loading:false
     })
 
 }
@@ -91,11 +128,11 @@ getCartTotal = () => {
     cartTotal = cartTotal + product.qty * product.price
   })
 
-  return '';
+  return cartTotal;
 }
 
   render(){
-       const {products} = this.state;
+       const {products,loading} = this.state;
       return (
         <div className="App">
           <NavBar  count={this.getcount()}  />
@@ -105,6 +142,7 @@ getCartTotal = () => {
               decreaseQuantity={this.handleDecrease}  
               ondelete={this.handleDelete}  
           />
+           {loading && <h1> Loading Products... </h1>}
            <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
         </div>
       );
